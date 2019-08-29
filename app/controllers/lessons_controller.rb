367c70249c8a7202c12938119f073f97e816d370
Stephen Cowen <sclc_mail@yahoo.com>
2019-08-29 01:28:50 +0000

    stripe1

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -7,6 +7,13 @@ class LessonsController < ApplicationController
 
   private
 
+  def require_authorized_for_current_lesson
+    if current_lesson.course.user != current_user
+      return render plain: 'Unauthorized', status: :unauthorized
+    end
+  end
+
+
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
